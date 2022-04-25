2ff98e990eca74a854a3583a7b4f08c90a1b6f9d
Chloe <roberts.chloe1996@gmail.com>
2022-04-25 18:51:59 -0500

    create files, start javascript

---
+++ java.js
@@ -0,0 +1,11 @@
+function computerplay() {
+
+}
+
+function playRound(playerselection, computerselection) {
+console.log(playerSelection)
+}
+const playerChoice ="rock"
+const comp = "paper"
+
+playRound(playerchoice, comp)
