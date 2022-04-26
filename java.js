b0715d377df13cc9242ab3e1a2f1c532dfd43cbc
Chloe <roberts.chloe1996@gmail.com>
2022-04-26 17:19:08 -0500

    added some functions

--- java.js
+++ java.js
@@ -1,11 +1,38 @@
+const choices = ["rock","paper","scissors"]
+
 function computerplay() {
+    playRound();
 
 }
 
-function playRound(playerselection, computerselection) {
-console.log(playerSelection)
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
 }
-const playerChoice ="rock"
-const comp = "paper"
 
-playRound(playerchoice, comp)
+function playerChoice() {
+    let input = prompt('type rock paper or fart')
+    while(input == null){
+        input = prompt("pick one of the words shithead")
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+       input = prompt("UwU")
+       input = input.toLowerCase();
+       check = validateInput;
+    }
+    
+    
+}
+
+function computerChoice() {
+    return choices[Math.floor(Math.random()*3)]
+}
+
+
+function validateInput(choice) {
+    return choices.includes(choice);
+
+}
+computerplay();
